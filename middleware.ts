78ed7a7e8702447c82f971e7d37111e0c5cd9046
Daniel Britto <121539061+db1211@users.noreply.github.com>
2024-06-20 15:25:39 +0530

    client-side

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const ProtectedRoute = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/previous",
+  "/personal-room",
+  "/meeting(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (ProtectedRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
